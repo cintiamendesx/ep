efe70ebf4c7f58043045e675fb9c037d18d51eae
cintiamendesx <cintiamendesx@gmail.com>
2022-07-16 16:25:49 -0300

    nextcon

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apis = {
   development: "http://localhost:3020/api",
-  production: "https://git.heroku.com/finalironlibrary.git/api",
+  production: "https://finalironlibrary.herokuapp.com/api",
 };
 
 // Pré-configurando a URL padrão do nosso backend em uma instância do Axios
